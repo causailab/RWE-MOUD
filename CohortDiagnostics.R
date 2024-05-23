4824b9c2c55498a0e79459e587d7d821c2b6e9c5
Ruochong Fan <63220231+SamuelFan1215@users.noreply.github.com>
2024-05-23 17:57:05 -0400

    Add files via upload

--- CohortDiagnostics.R
+++ CohortDiagnostics.R
@@ -26,23 +26,56 @@ library(RSQLite)
 library(DbDiagnostics)
 library(htmltools)
 
-exportFolder <- 'export'
+# Connect to database
+connectionDetails <- DatabaseConnector::createConnectionDetails(
+  connectionString = "your_string",
+  dbms = "your_dbms",
+  user = "user_name",
+  password = "super_secret", 
+  pathToDriver = "local/path/to/JDBCdriver")
+
+conn <- DatabaseConnector::connect(connectionDetails)
+cdmDatabaseSchema <- "" # the fully qualified database schema name of the CDM
+targetCohortTable <- "cohort" # cohort table name
+targetDatabaseSchema <- ""
+tempDatabaseSchema <- ""
+vocabularyDatabaseSchema <- ""
+cdmSourceName <- "" # a human readable name for your CDM source
+cdmVersion <- "" # the CDM version you are targeting. Currently supports 5.2, 5.3, and 5.4
+
+# Create cohort definition set
+baseUrl <- "http://api.ohdsi.org:80/WebAPI"
+cohortIds <- c(1789418, # methadone exposure
+               1789423, # buprenorphine exposure
+               1789470, # main outcome (OUD or opioid overdose)
+               1789694, # OUD outcome only
+               1789695  # opioid overdose outcome only
+)
+
+cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(
+  baseUrl = baseUrl,
+  cohortIds = cohortIds,
+  generateStats = TRUE
+)
+
+# Cohort diagnostics
+exportFolder <- 'choose/your/local/export/folder'
 
 CohortDiagnostics::executeDiagnostics(
   cohortDefinitionSet = cohortDefinitionSet,
   connectionDetails = connectionDetails,
   cohortTable = targetCohortTable,
-  cohortDatabaseSchema = cohortDatabaseSchema,
+  cohortDatabaseSchema = cdmDatabaseSchema,
   cdmDatabaseSchema = cdmDatabaseSchema,
   tempEmulationSchema = cdmDatabaseSchema,
   exportFolder = exportFolder,
-  databaseId = "", # human-readable database name
+  databaseId = "", # a human readable database name
   minCellCount = 5
 )
 
 CohortDiagnostics::createMergedResultsFile(
   dataFolder = exportFolder, 
-  sqliteDbPath = "MergedCohortDiagnosticsData.sqlite", # default working directory
+  sqliteDbPath = "MergedCohortDiagnosticsData.sqlite", 
   overwrite = TRUE
 )
 
